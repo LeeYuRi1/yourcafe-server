cba28e6c1d59474cc206e3c26ceb4204ba4e189b
권재원 <springkjw@gmail.com>
2019-04-02 19:12:15 +0900

    모듈 추가

--- src/database/models/Cafe.js
+++ src/database/models/Cafe.js
@@ -1,32 +1,28 @@
 // @flow
-import Sequelize from 'sequelize';
-import db from 'database/db';
-import { userInfo } from 'os';
+import Sequelize from "sequelize";
+import db from "database/db";
+import { userInfo } from "os";
 
 export interface CafeModel {
-  id: string,
-  name: string,
-  address: string,
+  id: string;
+  name: string;
+  address: string;
 }
 
-
-const Cafe = db.define(
-  'cafes',
-  {
-    id: {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV1,
-      primaryKey: true,   
-    },
-    name: Sequelize.STRING,
-    address: Sequelize.STRING,
-    long: Sequelize.FLOAT,
-    lat: Sequelize.FLOAT,
-    created_at: Sequelize.DATE,
-    updated_at: Sequelize.DATE,
-  }
-);
+const Cafe = db.define("cafes", {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV1,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  address: Sequelize.STRING,
+  long: Sequelize.FLOAT,
+  lat: Sequelize.FLOAT,
+  created_at: Sequelize.DATE,
+  updated_at: Sequelize.DATE
+});
 
 Cafe.associate = function() {
-  Cafe.hasOne
-}
+  Cafe.hasOne;
+};
